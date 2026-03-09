74d96e0cba2404154e3c5b6b7cda8e477218ff7a
Fadwabaali <82619118+fadwabaali@users.noreply.github.com>
2026-03-09 05:10:47 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXXoCL9leF18ojsWtoGor0uqHn2-0feIg",
-  authDomain: "iftar-8ef77.firebaseapp.com",
-  projectId: "iftar-8ef77",
-  storageBucket: "iftar-8ef77.firebasestorage.app",
-  messagingSenderId: "849159639861",
-  appId: "1:849159639861:web:1d1ba5f408a71ace26327c",
-  measurementId: "G-F10YLJ8YV6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
